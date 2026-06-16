3f6ff79a6d3b84c04bfb39449be1ff1430e070cc
ngochuyen <ngothingochuyen25022007@gmail.com>
2026-06-16 15:49:14 +0700

    moi

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import ManagerLayout from "./components/manager/ManagerLayout";
-import { publicRoutes } from "./routes/storeRoutes";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Route, Router, Routes } from "react-router";
+import { publicRoutes } from "./routes";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route element={<ManagerLayout />}>
-          {/* Route mặc định */}
-          <Route path="/" element={<Navigate to="/stores" replace />} />
-
-          {/* Các route */}
-          {publicRoutes.map((route, index) => {
-            const Page = route.component;
-
-            return (
-              <Route
-                key={index}
-                path={route.path}
-                element={<Page />}
-              />
-            );
-          })}
-        </Route>
+        {/* Tự động duyệt qua danh sách cấu hình để tạo tuyến đường */}
+        {publicRoutes.map((route, index) => {
+          const Page = route.component;
+          return (
+            <Route 
+              key={index} 
+              path={route.path} 
+              element={<Page />} 
+            />
+          );
+        })}
       </Routes>
     </Router>
   );
 }
-
 export default App;
