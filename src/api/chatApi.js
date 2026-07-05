aeee2ff6f52bfa29fb070f56dd0264cc42b5a3de
Le Thu Thao <lethithuthao@gmail.com>
2026-07-05 22:50:34 +0700

    chatApi

---
+++ src/api/chatApi.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+
+
+const API = axios.create({
+  baseURL: "https://tmdt-backend-ego0.onrender.com/api",
+});
+
+
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+
+  return config;
+});
+
+
+// ================= Danh sách hội thoại =================
+export const getConversations = (shopId) => {
+  return API.get("/chat/rooms", {
+    params: {
+      shop_id: shopId,
+    },
+  });
+};
+
+
+// ================= Lịch sử chat =================
+export const getChatHistory = (userId, shopId) => {
+  return API.get("/chat/history", {
+    params: {
+      user_id: userId,
+      shop_id: shopId,
+    },
+  });
+};
+
+
+// ================= Gửi tin nhắn =================
+export const sendMessage = ({
+    user_id,
+    shop_id,
+    content,
+    image_file = null,
+}) => {
+
+
+    if (image_file) {
+        const formData = new FormData();
+
+
+        formData.append("user_id", user_id);
+        formData.append("shop_id", shop_id);
+        formData.append("content", content);
+        formData.append("image_file", image_file);
+
+
+        return API.post("/chat/send", formData);
+    }
+
+
+    return API.post("/chat/send", {
+        user_id,
+        shop_id,
+        content,
+    });
+};
+
